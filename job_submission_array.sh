5970c98229d4f90f464dfe023e94f56afce7ab30
LiGuiye <1171116470@qq.com>
2023-07-16 12:07:16 -0500

    update sliceNum and final training command

--- job_submission_array.sh
+++ job_submission_array.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=dipS4          # create a short name for your job
+#SBATCH --job-name=dipS64          # create a short name for your job
 #SBATCH --partition=matador
 #SBATCH --nodes=1                # node count
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=20       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --array=0-19:1           # job array with index values
+#SBATCH --array=0-99:1           # job array with index values
 #%module load matador/0.15.4
 #%module load gcc/9.3.0
 #%module load cudnn/8.0.1.13-1.cuda11.0
@@ -17,6 +17,16 @@ echo "Executing on the machine:" $(hostname)
 
 . $HOME/conda/etc/profile.d/conda.sh
 conda activate pytorch_gpu
-# python super-resolution-rs.py --type Wind --factor 4 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process extract --savePath extract32_iter6k
 
-python super-resolution-rs.py --type Solar --factor 4 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process extract --savePath extract32_iter6k
+# python super-resolution-rs.py --type Wind --factor 4 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process extract --savePath extract32_iter6k
+# python super-resolution-rs.py --type Wind --factor 10 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process resize --savePath resize10x_iter6k
+# python super-resolution-rs.py --type Wind --factor 50 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process resize --savePath resize50x_iter6k
+# python super-resolution-rs.py --type Wind --factor 64 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process resize --savePath resize64x_iter6k
+
+
+# python super-resolution-rs.py --type Solar --factor 4 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process extract --savePath extract32_iter6k
+# python super-resolution-rs.py --type Solar --factor 5 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process resize --savePath resize5x_iter6k
+# python super-resolution-rs.py --type Solar --factor 25 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process resize --savePath resize25x_iter6k
+# python super-resolution-rs.py --type Solar --factor 32 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process resize --savePath resize32x_iter6k
+python super-resolution-rs.py --type Solar --factor 64 --slice $SLURM_ARRAY_TASK_ID --num_iter 6000 --process resize --savePath resize64x_iter6k --slicesNum 100
+
